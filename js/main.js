39a2144485738254090964183fc655eb067f3d33
Gillespie <n0164225@libp45p-26859dy.lm.lmig.com>
2017-03-13 15:42:33 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [ ];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
+var checkForMatch = function () {
+
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match !");
+		} else {
+			alert("Sorry, try again.");
+		};
+	} else {
+		// do nothing
+	};
+};
+
+var flipCard = function (cardId) {
+
+	console.log("User flipped over a " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+
+};
+
+flipCard(0);
+flipCard(2);
+
+/*
+	var cardOne = cards[0];
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardOne);
+	cardsInPlay.push(cardTwo);
+
+console.log("User flipped queen");
+console.log("User flipped king");
 
 if (cardsInPlay.length === 2) {
 	if (cardOne === cardTwo) {
@@ -15,5 +45,6 @@ if (cardsInPlay.length === 2) {
 } else {
 	// take this path
 };
-console.log("User flipped queen");
-console.log("User flipped king");
+*/
+
+
